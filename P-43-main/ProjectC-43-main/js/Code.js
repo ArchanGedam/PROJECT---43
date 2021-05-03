3eaf8fbf99ed89b55240aee3555441c08288b19a
ArchanGedam <75840359+ArchanGedam@users.noreply.github.com>
2021-05-03 18:58:57 +0530

    Add files via upload

---
+++ P-43-main/ProjectC-43-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues(){   
+  fill ("white");
+  textSize(15);
+  text("AIBELRVA",100,50)
+fill("lightblue")
+text("hint:always changing , not constant",100,70)
+
+fill ("white");
+textSize(15);
+text("FCOTIUNC",700,150)
+fill("lightblue")
+text("hint:performs a perticular task",700,170)
+
+fill ("white");
+textSize(15);
+text("ATASBAED",100,250)
+fill("lightblue")
+text("hint:stores information",100,270)
+
+}
